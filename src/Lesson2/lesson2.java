b2498915621df1fae1c30f0c7ba6942f8130f7eb
pavelnazaro <pavelnazaro@mail.ru>
2019-10-09 11:23:35 +0300

    Task1

--- src/Lesson2/lesson2.java
+++ src/Lesson2/lesson2.java
@@ -1,4 +1,18 @@
 package Lesson2;
 
 public class lesson2 {
+    public static void main(String[] args) {
+        String[][] mas = new String[4][4];
+
+        newMethodTest(mas);     //Вызываем метод по заданию
+    }
+
+    private static void newMethodTest(String[][] mas) {
+        //Задание 1
+        System.out.println("Задание 1:");
+        if (mas.length != 4 || mas[0].length != 4)
+            throw new MyArraySizeException();           //Бросаем исключение неверного размера массива
+        else
+            System.out.println("Все ок!");
+    }
 }
