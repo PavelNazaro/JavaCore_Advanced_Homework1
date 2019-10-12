ffd58a53ee93400a6f02468f167e418d984cc955
pavelnazaro <pavelnazaro@mail.ru>
2019-10-12 12:26:26 +0300

    modify

--- src/Lesson3/Task1.java
+++ src/Lesson3/Task1.java
@@ -9,31 +9,15 @@ public class Task1 {
                 "blue", "cat", "cat", "blue", "blue", "red", "green", "dog", "blue"));  //Вносим 15 значений
         System.out.println("Массив слов:\n" + listOfWords);
 
-        //Создаем Сет и добаляем в него все значения из listInteger
-        //Добавятся только уникальные значения
-        Set<String> list1 = new HashSet<>();
-        list1.addAll(listOfWords);
-        System.out.println("Массив уникальных значений:\n" + list1);
-
-        //Создаем мар чтобы показать, сколько раз встречается каждое слово
-        Map<String, Integer> mapOfWordsAmount = new HashMap<>();
-        mapOfWordsAmount = counting(listOfWords, list1);
-        System.out.println("Количество повторений каждого слова:\n" + mapOfWordsAmount);
+        //Создаем мар чтобы вывести уникальные слова и их кол-во
+        Map<String, Integer> mapOfWordsAmount = counting(listOfWords);
+        System.out.println("Уникальные слова и их количество повторений:\n" + mapOfWordsAmount);
     }
 
-    private static Map<String, Integer> counting(List<String> listOfWords, Set<String> list1) {
+    private static Map<String, Integer> counting(List<String> listOfWords) {
         Map<String, Integer> mapOfWordsAmount = new HashMap<>();
         for (String a : listOfWords) {
-            for (String b : list1) {
-                if (a.equals(b)) {
-                    try {       //Если в мапе уже есть такая запись, то прибавляем к вэлью 1
-                        mapOfWordsAmount.put(b,mapOfWordsAmount.get(b)+1);
-                    } catch (Exception e) {
-                        mapOfWordsAmount.put(b,1);      //Если нет то добавляем новую запись
-                    }
-                    break;
-                }
-            }
+            mapOfWordsAmount.merge(a, 1, Integer::sum);      //Добавляем новую запись, если такой нет, либо прибавляем единицу кол-ву
         }
         return mapOfWordsAmount;
     }
