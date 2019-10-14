2c85b2a20823a758cec45451963af68e3b3f9e7d
pavelnazaro <pavelnazaro@mail.ru>
2019-10-14 13:00:10 +0300

    modify

--- src/Lesson3/Task2.java
+++ src/Lesson3/Task2.java
@@ -4,31 +4,32 @@ import java.util.*;
 
 public class Task2 {
     public static void main(String[] args) {
-        Map<Integer, String> phoneBook = new HashMap<>();
+        Map<String, List<Integer>> phoneBook = new HashMap<>();
         addNamesAndPhones(phoneBook);
 
         //Вывод телефонной книги:
         System.out.println(phoneBook);
 
         //Поиск номера телефона по фамилии:
-        findPhoneByName(phoneBook, "Смирнов");
-        findPhoneByName(phoneBook, "Сидоров");
+        System.out.println("Номер телефона по фамилии Смирнов: " + phoneBook.get("Смирнов"));
+        System.out.println("Номер телефона по фамилии Сидоров: " + phoneBook.get("Сидоров"));
     }
 
-    private static void addNamesAndPhones(Map<Integer, String> phoneBook) {
-        phoneBook.put(5444644, "Иванов");
-        phoneBook.put(3469831, "Смирнов");
-        phoneBook.put(4433685, "Сидоров");
-        phoneBook.put(3456787, "Сидоров");
-        phoneBook.put(2345677, "Петров");
+    private static void addNamesAndPhones(Map<String, List<Integer>> phoneBook) {
+        putPhoneBook(phoneBook, "Иванов", 5444644);
+        putPhoneBook(phoneBook, "Смирнов", 3469831);
+        putPhoneBook(phoneBook, "Сидоров", 4433685);
+        putPhoneBook(phoneBook, "Сидоров", 3456787);
+        putPhoneBook(phoneBook, "Петров", 2345677);
     }
 
-    private static void findPhoneByName(Map<Integer, String> phoneBook, String name) {
-        for (int key : phoneBook.keySet()){
-            String value = phoneBook.get(key);
-            if (value.equals(name)){
-                System.out.println("Номер телефона по фамилии '" + name + "': " + key);
-            }
+    private static void putPhoneBook(Map<String, List<Integer>> phoneBook, String name, int number) {
+        List<Integer> getPhoneBookValue = phoneBook.get(name);
+        List<Integer> list = new ArrayList<>();
+        list.add(number);
+        if (getPhoneBookValue != null) {
+            list.addAll(getPhoneBookValue);
         }
+        phoneBook.put(name, list);
     }
 }
